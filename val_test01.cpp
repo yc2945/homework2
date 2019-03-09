5c5ea8edc2bf03859fadbba02d855c3af6058cfb
yc2945 <yc2945@nyu.edu>
2019-03-09 18:18:48 -0500

    update test-1

--- val_test01.cpp
+++ val_test01.cpp
@@ -77,13 +77,15 @@ void f ( int n )
   x[1] = 1;
   cout << "  " << 1 << "  " << x[1] << "\n";
 
-  for ( i = 2; i <= n; i++ )
+  // We only allocated space for size n, so the maximum accessible index should be n-1. 
+  for ( i = 2; i < n; i++ )
   {
     x[i] = x[i-1] + x[i-2];
     cout << "  " << i << "  " << x[i] << "\n";
   }
 
-  delete [] x;
+  // Should use free to free space allocatd by malloc. Using delete would cause undefined bahavior. 
+  free(x); 
 
   return;
 }
